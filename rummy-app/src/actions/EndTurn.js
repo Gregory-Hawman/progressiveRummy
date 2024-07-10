dbca4f7566615387e375a32f87f153c4e51f821f
Gregory Hawman <g.hawman@gmail.com>
2024-07-09 21:52:21 -0400

    switched from CRA to Vite

--- rummy-app/src/actions/EndTurn.js
+++
@@ -1,7 +0,0 @@
-function EndTurn() {
-    // end current players turn and move to the next player
-    // if current player can go out, the run last card phase, else move to next player.
-    if (playersList.currentPlayer){
-  
-    }
-  }
